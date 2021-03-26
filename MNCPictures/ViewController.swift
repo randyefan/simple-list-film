7264af36a2780ab06b48165094d266def7598868
Randy Efan <randy@mamiteam.com>
2021-03-26 23:34:52 +0900

    Initial Commit

---
+++ MNCPictures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MNCPictures
+//
+//  Created by Randy Efan Jayaputra on 26/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
